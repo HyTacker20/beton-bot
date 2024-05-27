444e9aaaf3eb955bbc644aa93bcfd35f4cdefd66
Andrii Hermak <hermak20102004@gmail.com>
2024-05-27 21:56:56 +0200

    feat: remove running with args

--- src/mypackage/__init__.py
+++ src/mypackage/__init__.py
@@ -1,24 +1,20 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
 
 from .bot import setup_bot, launch_bot
 from .bot.api import setup_google_sheet_api, setup_google_maps_api
-from .cli import define_arg_parser
 from .config import load_config
 from .db import setup_session_maker
 from .logger import setup_logger
 from .webhook import setup_app, Application
 
-
 load_dotenv()
 
 
 def webhook_app() -> Application:
-    # this function is just an example of how you can create the app factory
     config_path = os.environ.get('CONFIG_PATH', 'config.toml')
-    use_env_vars = os.environ.get('CONFIG_USE_ENV_VARS', False) in ('1', 'true', 'True', 'TRUE')
+    use_env_vars = os.environ.get('CONFIG_USE_ENV_VARS', 'false').lower() in ('1', 'true', 'True', 'TRUE')
     config_env_mapping_path = os.environ.get('CONFIG_ENV_MAPPING_PATH', 'config_env_mapping.toml')
     cfg = load_config(config_path, use_env_vars, config_env_mapping_path)
 
@@ -30,19 +26,14 @@ def webhook_app() -> Application:
     app.ctx.secret_token = cfg.bot.webhook.secret_token
     app.ctx.logger = setup_logger(cfg.logger)
 
-    # use_webhook is intentionally hard-coded to True here as we're using webhook
     launch_bot(bot_, cfg.bot.drop_pending, True, cfg.bot.allowed_updates, cfg.bot.webhook)
     return app
 
 
 def main():
-    # If you wish to use webhook, you'll probably want to launch the bot using the web-server, e.g. gunicorn
-    # In this case you should be calling the webhook_app_factory function
-    arg_parser = define_arg_parser()
-    args = arg_parser.parse_args()
-    config_path = os.environ.get('CONFIG_PATH', args.config_path)
-    use_env_vars = os.environ.get('CONFIG_USE_ENV_VARS', args.use_env_vars) in ('1', 'true', 'True', 'TRUE', True)
-    config_env_mapping_path = os.environ.get('CONFIG_ENV_MAPPING_PATH', args.config_env_mapping_path)
+    config_path = os.environ.get('CONFIG_PATH', 'config.toml')
+    use_env_vars = os.environ.get('CONFIG_USE_ENV_VARS', 'false').lower() in ('1', 'true', 'True', 'TRUE')
+    config_env_mapping_path = os.environ.get('CONFIG_ENV_MAPPING_PATH', 'config_env_mapping.toml')
 
     bot_token = os.environ.get('TOKEN')
     cfg = load_config(config_path, use_env_vars, config_env_mapping_path)
@@ -55,6 +46,11 @@ def main():
     google_sheet_api = setup_google_sheet_api(cfg.google_sheet_api, db_session_maker, db_logger)
     google_maps_api = setup_google_maps_api(os.environ.get("GOOGLE_MAPS_API_KEY"))
 
-    bot_ = setup_bot(cfg.bot, db_session_maker, db_logger, google_sheet_api, google_maps_api, cfg.messages, cfg.buttons, bot_logger)
+    bot_ = setup_bot(cfg.bot, db_session_maker, db_logger, google_sheet_api, google_maps_api, cfg.messages, cfg.buttons,
+                     bot_logger)
 
     launch_bot(bot_, cfg.bot.drop_pending, False, cfg.bot.allowed_updates, cfg.bot.webhook)
+
+
+if __name__ == "__main__":
+    main()
