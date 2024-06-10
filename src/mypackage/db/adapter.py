2286197251d266277999375fe886523489fd535b
Andrii Hermak <hermak20102004@gmail.com>
2024-06-10 21:39:08 +0200

    Implement methods for managing producers and discounts in adapter
    - Added new methods in adapter for managing producers and discounts.

--- src/mypackage/db/adapter.py
+++ src/mypackage/db/adapter.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import Session
 
 from .dto import NewUserDTO, DispatchPointDTO
 from .exceptions import DBError
-from .models import User
-from .operations import user, dispatch_points, database
+from .models import User, Producer
+from .operations import user, dispatch_points, database, producer
 
 
 class DBAdapter:
@@ -28,6 +28,9 @@ class DBAdapter:
     def get_user(self, tg_user_id: int) -> Optional[User]:
         return self._session_wrapper(user.get, tg_user_id)
 
+    def get_user_with_discounts(self, tg_user_id: int) -> Optional[User]:
+        return self._session_wrapper(user.get_with_discounts, tg_user_id)
+
     def get_all_users(self, offset: int, limit: int) -> Iterable[User]:
         return self._session_wrapper(user.get_all, offset, limit)
 
@@ -37,8 +40,12 @@ class DBAdapter:
     def add_user(self, new_user_dto: NewUserDTO):
         return self._session_wrapper(user.add, new_user_dto)
 
-    def update_user_discount(self, tg_user_id: int, concrete_discount: int, delivery_discount: int) -> bool:
-        return self._session_wrapper(user.update_discount, tg_user_id, concrete_discount, delivery_discount)
+    def update_user_discount(self, tg_user_id: int, producer_title: str,
+                             concrete_discount: int, delivery_discount: int,
+                             concrete_discount_vat: int, delivery_discount_vat: int) -> bool:
+        return self._session_wrapper(user.update_discount, tg_user_id, producer_title,
+                                     concrete_discount, delivery_discount,
+                                     concrete_discount_vat, delivery_discount_vat)
 
     def check_if_user_is_admin(self, tg_user_id: int) -> bool | None:
         return self._session_wrapper(user.is_admin, tg_user_id)
@@ -66,3 +73,27 @@ class DBAdapter:
 
     def check_and_create_tables(self):
         return self._session_wrapper(database.check_and_create_tables)
+
+    def add_producer(self, title: str):
+        return self._session_wrapper(producer.add, title)
+
+    def get_producer_by_id(self, _id: int) -> Producer:
+        return self._session_wrapper(producer.get_by_id, _id)
+
+    def get_producer_by_title(self, title: str) -> Producer:
+        return self._session_wrapper(producer.get_by_title, title)
+
+    def get_all_producers(self):
+        return self._session_wrapper(producer.get_all)
+
+    def get_all_producer_titles(self):
+        return self._session_wrapper(producer.get_all_titles)
+
+    def get_producer_or_create(self, title: str):
+        return self._session_wrapper(producer.get_or_create, title)
+
+    def delete_all_producers_by_title(self, titles: Iterable[str]):
+        return self._session_wrapper(producer.delete_all_by_title, titles)
+
+    def sync_producers(self, titles: Iterable[str]):
+        return self._session_wrapper(producer.sync_producers, titles)
